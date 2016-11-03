26327c61fe234f968c0a8098f42a7ab7e46df96f
DESKTOP-VLTJDR9\Marlene Macdonald <marmac@gmail.com>
2016-11-03 11:37:43 -0700

    Final version - 11/3/2016

--- src/app/pages/charts/chart.js
+++ src/app/pages/charts/chart.js
@@ -1,4 +1,3 @@
-'use strict';
 
 var WaveChart = angular.module('WaveChart', ["highcharts-ng"]);
 
@@ -8,100 +7,131 @@ var waveData = {
         pauwela: "assets/wave-pauwela.json",
         hanalei: "assets/wave-hanalei.json"
     }
+  
 
+WaveChart.factory('waveService', function($http, $q) {
 
+    var getWaveDataFunction = function(beach){
+        
+        var defer = $q.defer();
 
-WaveChart.factory('waveService', function($http) {
-    
-        var getWaveDataFunction = function(beach){
+        $http.get(waveData[beach])
+          .success(function(data) {
 
-            $http.get(waveData[beach])
-              .success(function(data) {
-                console.log('yay it works');
-                return data;
+            var Time = [];
+            for (var i = 0; i < data.length; i++) {
+                Time.push(data[i].Time);   
+            }
 
-              var Time = [];
-                for (var i = 0; i < data.length; i++) {
-                    Time.push(data[i].Time);   
-                }
+            var Output = [];
+            for (var i = 0; i < data.length; i++) {
+                Output.push(data[i].Output);
+            }
+        
+            defer.resolve({ time: Time, output: Output});
 
-                var Output = [];
-                for (var i = 0; i < data.length; i++) {
-                    Output.push(data[i].Output);
-                }
-                //console.log(Time);
-                //console.log(Output);
+        });
+        
+        return defer.promise;
+    }
 
-            });
-        }
-
-        return {
-            getWaveData: getWaveDataFunction
-        };
-   
+    return {
+        getWaveData: getWaveDataFunction
+    };
 });      
 
 WaveChart.controller('chartCtrl', function ($scope, waveService, $state) {
-    
-        
-        var currentBeach = $state.current.title.toLowerCase();
-        $scope.data = waveService.getWaveData(currentBeach);
-
-        console.log($scope.data);
 
 
-            $scope.chartConfig = {
-                title: {
-                    text: 'Wave Height Observations'
+    var currentBeach = $state.current.title.toLowerCase();
+
+    waveService.getWaveData(currentBeach)
+        .then(function(waveData){
+            $scope.waveData = waveData;
+
+             $scope.chartConfig = {
+                    title: {
+                        text: 'Wave Height Observations'
+                    },
+                    subtitle: {
+                        text: 'according to the PacIOOS'
+                    },
+                    options: {
+                        chart: {
+                            type: 'spline'
+                        },
+                        plotOptions: {
+                            spline: {
+                                lineWidth: 2,
+                                states: {
+                                    hover: {
+                                        lineWidth: 3
+                                    }
+                                },
+                                marker: {
+                                    enabled: false
+                                }
+                            },
+                            area: {
+                            fillColor: {
+                                linearGradient: { x1: 0, y1: 0},
+                                stops: [
+                                    [0, Highcharts.getOptions().colors[0]],
+                                    [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                                ]
+                            },
+                            marker: {
+                                enabled: true
+                            },
+                            lineWidth: 1,
+                            states: {
+                                hover: {
+                                    lineWidth: 2
+                                }
+                            },
+                            threshold: null
+                            }
+                        }
+                    },
+                    xAxis: {
+                    name: "Time", 
+                    categories: waveData.time  
+                   
                 },
-                subtitle: {
-                    text: 'according to the PacIOOS'
-                },
-                options: {
-                    chart: {
-                        type: 'spline'
+                    yAxis: {
+                        title: {
+                            text: 'Wave Height'
+                        },
+                        labels: {
+                            formatter: function () {
+                                return this.value;
+                            }
+                        }
+                    },
+                    tooltip: {
+                        crosshairs: true,
+                        shared: true
                     },
                     plotOptions: {
                         spline: {
-                            lineWidth: 2,
-                            states: {
-                                hover: {
-                                    lineWidth: 3
-                                }
-                            },
                             marker: {
-                                enabled: false
+                                radius: 4,
+                                lineColor: '#666666',
+                                lineWidth: 1
                             }
-                        },
-                        area: {
-                        fillColor: {
-                            linearGradient: { x1: 0, y1: 0},
-                            stops: [
-                                [0, Highcharts.getOptions().colors[0]],
-                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                            ]
-                        },
-                        marker: {
-                            enabled: false
-                        },
-                        lineWidth: 1,
-                        states: {
-                            hover: {
-                                lineWidth: 2
-                            }
-                        },
-                        threshold: null
                         }
-                    }
-                },
-                xAxis: [{
-                name: "Time",   
-                categories: ['00:09', '00:39', '01:09', '01:39']
-            }],
-            series: [{
-                name: "Wave Height",
-                data: [4.82, 4.63, 5.52, 5.96]
-            }]
-        }
+                    },
+                    series: [{
+                        name: 'Wave Height',
+                        marker: {
+                            symbol: 'square'
+                        },
+                        data: waveData.output
+                       
+                    }]
+                }
+
+        });
+          
 });
 
